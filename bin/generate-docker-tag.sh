d93480cd2bc5f4d0ba3d93a0ce134b174d7ab950
Jonathan Garbee <jonathan@garbee.me>
2026-01-17 17:46:40 -0500

    improve tagging. Try to get summary output for audits

--- bin/generate-docker-tag.sh
+++ bin/generate-docker-tag.sh
@@ -2,6 +2,11 @@
 
 # Generate a Docker tag based on the current date, event type, and unique identifiers.
 # This allows for easy identification of when and why a Docker image was built.
+# For schedules, we use the year and week number. These are our automated production builds. Keeping a stable tag unique to each one is important.
+# For pushes, we use the short SHA of the commit. This allows us to test images against the specific commits.
+# For manual runs, we use a combination of run number, attempt, and timestamp to ensure uniqueness globally.
+# For pull requests, we use the PR number for easy identification. These we don't want to get more unique since they are temporary anyways.
+# For anything unhandled, we fall back to a combination of run number and attempt. This is good enough if any other events happen until we can account for them.
 
 set -euoC pipefail
 
@@ -12,6 +17,7 @@ set -euoC pipefail
 : "${RUN_ATTEMPT:?RUN_ATTEMPT must be set}"
 : "${SHA:?SHA must be set}"
 : "${GITHUB_OUTPUT:?GITHUB_OUTPUT must be set}"
+: "${PR_NUMBER:-}"
 
 
 case "$EVENT_NAME" in
@@ -25,6 +31,9 @@ case "$EVENT_NAME" in
   workflow_dispatch)
     UNIQUE="${RUN_NUMBER}_${RUN_ATTEMPT}_$(date -u +"%Y-%m-%d-%H-%M-%S")"
     TAG="on.manual-${UNIQUE}"
+    ;;
+  pull_request)
+    TAG="on.pr-${PR_NUMBER}"
     ;;
   *)
     TAG="on.${EVENT_NAME}-${RUN_NUMBER}_${RUN_ATTEMPT}"
